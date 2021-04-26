f97f3885251e2c3aecdaab3a4e52d8bc11095cc4
Shashank sharma <78837537+theshashankk@users.noreply.github.com>
2021-04-27 02:26:26 +0530

    Update urls.py

--- djangotry/hardcore/home/urls.py
+++ djangotry/hardcore/home/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("joinus",views.joinuss, name='join us'),
     path("contactus",views.contactus, name='contact us'),
     path("rulesguidelines",views.rulesguidelines, name='rules & guidelines'),
-    path("verifiedusers",views.verifiedusers, name='verified users'),
+    path("moderator",views.verifiedusers, name='moderator'),
     path("home",views.home, name='home'),
     path("",views.home, name='home')
     
